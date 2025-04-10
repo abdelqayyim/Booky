d6eedf5a6fb3c32d240cd5be8d6f95f0fb1eef03
abdelqayyim <abdelymaster@gmail.com>
2025-04-10 16:34:27 -0400

    Redux and tailwind initial configuration.

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// src/store/store.js
+import { configureStore } from '@reduxjs/toolkit';
+// import other reducers when needed
+
+const store = configureStore({
+  reducer: {
+    // add more slices here
+  },
+});
+
+export default store;
