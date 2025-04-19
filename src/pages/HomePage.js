2abdb0635baf095e89e06d99031ceb70845571f4
abdelqayyim <abdelymaster@gmail.com>
2025-04-19 18:41:08 -0400

    Building Home Page.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,83 @@
 import React from 'react'; 
+import { CHECKED_CALENDAR_SVG, METRICS_UP_SVG, ACTIVE_USERS_SVG, STOREFRONT_SVG, DOLLAR_SIGN_SVG } from './constants';
 
-const HomePage = (props)=>{
+const HomePage = (props) => {
+    const stats = [
+        {
+            title: "Total Bookings",
+            logo: CHECKED_CALENDAR_SVG,
+            metric: "12.5% from last month",
+            color: "#4f46e5",
+            backgroundColor: "rgba(79,70,229,0.1)"
+        },
+        {
+            title: "Active Users",
+            logo: ACTIVE_USERS_SVG,
+            metric: "12.5% from last month",
+            color: "#10b981",
+            backgroundColor: "rgba(16, 185, 129, 0.1)"
+        },
+        {
+            title: "Active Providers",
+            logo: STOREFRONT_SVG,
+            metric: "12.5% from last month",
+            color: "#f59e0b",
+            backgroundColor: "rgba(245, 158, 11, 0.1)"
+        },
+        {
+            title: "Revenue",
+            logo: DOLLAR_SIGN_SVG,
+            metric: "12.5% from last month",
+            color: "#ef4444",
+            backgroundColor: "rgba(239, 68, 68, 0.1)"
+        }
+    ]
+    const StatCard = ({card}) => {
+        return (
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-[20px] mb-[25px]">
+                <div className='bg-white w-fit rounded-[10px] p-[20px] shadow-[0_2px_5px_rgba(0,0,0,0.05)] transition-all duration-300 hover:-translate-y-[5px] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.1)]'>
+                    <div className="flex justify-between items-center mb-[15px]">
+                        <div className='text-[var(--secondary)] text-[14px] font-semibold'>
+                            { card.title}
+                        </div>
+                        <div className="flex flex-col items-center justify-center rounded text-[var(--primary)] w-[40px] h-[40px]" style={{color: card.color, backgroundColor: card.backgroundColor}}>
+                            {card.logo}
+                        </div>
+                    </div>
+                    <div className='text-[24px] font-bold mb-[5px]'>8,492</div>
+                    <div className='text-[var(--success)] flex flex-row gap-[2px] text-sm items-center'>
+                        <div>{METRICS_UP_SVG}</div><span>{ card.metric}</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
-        <div className='bg-orange-200 h-full'>
-            This is the homepage
+        <div className='bg-orange-200 h-full p-[25px]'>
+            {/* Page Title and Filter */}
+            <div className='mb-[20px] bg-blue-200'>
+                <h1 className='text-[24px] font-bold'>Dashboard</h1>
+                <button></button>
+            </div>
+
+            {/* Stats Grid */}
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-[20px] mb-[25px]">
+                {/* <div className='bg-white w-fit rounded-[10px] p-[20px] shadow-[0_2px_5px_rgba(0,0,0,0.05)] transition-all duration-300 hover:-translate-y-[5px] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.1)]'>
+                    <div className="flex justify-between items-center mb-[15px]">
+                        <div className='text-[var(--secondary)] text-[14px]'>
+                            Total Bookings
+                        </div>
+                        <div className="bg-[rgba(79,70,229,0.1)] text-[var(--primary)]">
+
+                        </div>
+                    </div>
+                    <div className='text-[24px] font-bold mb-[5px]'>8,492</div>
+                    <div className='text-[var(--success)] flex flex-row gap-[5px]'>
+                        <div>arrow</div><span>12.5% from last month</span>
+                    </div>
+                </div> */}
+                {stats.map((card) => <StatCard card={ card} />)}
+            </div>
         </div>
     )
 };
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { CHECKMARK_SVG, SEARCH_SVG, SEND_SVG } from './constants';
+import { ARROW_RIGHT, BELL_SVG, CHECKMARK_SVG, DARK_MODE_SVG, SEARCH_SVG, SEND_SVG } from './constants';
 
 const Navbar = (props) => {
   const SearchBar = () => {
@@ -14,17 +14,26 @@ const Navbar = (props) => {
     <div className='bg-white w-full h-[50px] px-[25px] py-[5px] flex flex-row'>
       <div className='flex flex-row'>
         <SearchBar /> 
-        <button className='mr-4 bg-[var(--primary)] hover:bg-[var(--primary-hover)] flex flex-row text-white py-[8px] px-[16px] rounded-[8px] border-none font-[500] cursor-pointer items-center gap-[8px] transition-colors duration-200 text-xs'>
+        <button className='mr-4 bg-[var(--primary)] hover:bg-[var(--primary-hover)] flex flex-row text-white px-[10px] rounded-[8px] border-none font-[500] cursor-pointer items-center gap-[8px] transition-colors duration-200 text-xs'>
           {CHECKMARK_SVG}
           Approve Provider
         </button>
-        <button className='bg-transparent hover:bg-[#dde0e4] flex flex-row text-[var(--secondary)] py-[8px] px-[16px] rounded-[8px] border border-[var(--secondary)] font-[500] cursor-pointer items-center gap-[8px] transition-colors duration-200 text-xs'>
+        <button className='bg-transparent hover:bg-[#dde0e4] flex flex-row text-[var(--secondary)] px-[10px] rounded-[8px] border border-[var(--secondary)] font-[500] cursor-pointer items-center gap-[8px] transition-colors duration-200 text-xs'>
           <div>{SEND_SVG}</div>
           Send Notification
         </button>
       </div>
-      <div className='flex-1 bg-red-200'>
-        New one
+      <div className='flex-1 flex flex-row justify-end'>
+        <div className='w-fit p-2 mr-2  hover:cursor-pointer'>{DARK_MODE_SVG}</div>
+        <div className='w-fit p-2 mr-2  hover:cursor-pointer'>{BELL_SVG}</div>
+        <div className='w-fit flex flex-row items-center p-[5px] rounded hover:bg-gray-200 hover:cursor-pointer'>
+          <div className='w-[35px] h-[35px] rounded-full bg-[var(--primary)] text-white flex flex-col items-center justify-center'>A</div>
+          <div className='mx-2'>
+            <div className='text-[14px] font-bold'>Admin User</div>
+            <div className='text-[var(--secondary)] text-[12px]'>System Admin</div>
+          </div>
+          <div className='rotate-90'>{ ARROW_RIGHT}</div>
+        </div>
       </div>
     </div>
   )
