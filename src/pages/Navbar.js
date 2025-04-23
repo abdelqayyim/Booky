c984b14d3e20c982a868819a0079c02d1264e4e3
abdelqayyim <abdelymaster@gmail.com>
2025-04-22 22:14:30 -0400

    Working on Mobile Navbar.

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,22 +1,39 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 import { ARROW_RIGHT, BELL_SVG, CHECKMARK_SVG, DARK_MODE_SVG, HAMBURGER_MENU_SVG, SEARCH_SVG, SEND_SVG } from './constants';
-
+import { Menu as MenuIcon, Close as CloseIcon } from '@mui/icons-material'; 
 const Navbar = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const SearchBar = () => {
     return (
-      <div className='hidden md:flex md:w-[300px] lg:w-[300px] flex-row items-center px-2 bg-[var(--light)] rounded md:mr-4 lg:mr-4'>
+      <div className='md:flex md:w-[300px] lg:w-[300px] flex-row items-center px-2 bg-[var(--light)] rounded md:mr-4 lg:mr-4'>
         <div className='mr-1 text-[var(--secondary)]'>{SEARCH_SVG}</div>
         <input className='text-[var(--dark)] bg-transparent border-none p-[5px] w-full outline-none' placeholder='Search for users or providers...'/>
       </div>
     )
   }
   return (
-    <div className='bg-white w-full h-[50px] md:px-[25px] lg:px-[25px] lg:py-[5px]  md:py-[5px] flex flex-row overflow-x-hidden'>
-      <div className='h-full w-[50px] mr-[5px] flex flex-col items-center justify-center'>
-        {HAMBURGER_MENU_SVG}
+    <div className='bg-white w-full h-[50px] px-0 py-0 md:px-[25px] md:py-[5px] flex flex-row overflow-x-hidden'>
+      <div className='flex flex-row items-center justify-between md:hidden w-full z-50 relative'>
+          {/* For Mobile View */}
+          <div className='h-full w-[50px] mr-[5px] flex flex-col items-center justify-center' onClick={()=>{setIsModalOpen(prev=>!prev)}}>
+          <MenuIcon />
+        </div>
+        {!isModalOpen && <div className='text-bold'>Booky </div>}
+        {!isModalOpen && <div className='w-[35px] h-[35px] rounded-full bg-[var(--primary)] text-white flex flex-col items-center justify-center'>A</div>
+        }
+        
+        {/* Menu Drop Down */}
+         {/* Dropdown Overlay */}
+         <div className={`bg-red-200 fixed top-0 left-0 w-64 h-full shadow-lg transform ${isModalOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-50`}>
+         <div className='w-full bg-gray-300 flex flex-row justify-end px-[5px]' onClick={()=>{setIsModalOpen(false)}}><CloseIcon/></div>
+</div>
+
       </div>
       
-      <div className='flex flex-row'>
+
+      <div className='hidden md:flex flex-1 flex-row justify-between items-center'>
+        {/* For larger screen view */}
+        <div className='flex flex-row'>
         <SearchBar />
         <button className='mr-4 bg-[var(--primary)] hover:bg-[var(--primary-hover)] flex flex-row text-white px-[10px] rounded-[8px] border-none font-[500] cursor-pointer items-center gap-[8px] transition-colors duration-200 text-xs'>
           {CHECKMARK_SVG}
@@ -28,7 +45,7 @@ const Navbar = (props) => {
         </button>
       </div>
 
-      {/* <div className='flex-1 flex flex-row justify-end'>
+      <div className='flex-1 flex flex-row justify-end'>
         <div className='w-fit p-2 mr-2  hover:cursor-pointer'>{DARK_MODE_SVG}</div>
         <div className='w-fit p-2 mr-2  hover:cursor-pointer'>{BELL_SVG}</div>
         <div className='w-fit flex flex-row items-center p-[5px] rounded hover:bg-gray-200 hover:cursor-pointer'>
@@ -39,7 +56,9 @@ const Navbar = (props) => {
           </div>
           <div className='rotate-90'>{ ARROW_RIGHT}</div>
         </div>
-      </div> */}
+      </div>
+      </div>
+      
     </div>
   )
 };
